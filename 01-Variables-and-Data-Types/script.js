5e3b796af7d5b3ce381ddbbe69bd4bcdfd54670f
Vivek Kumar Rana <vivekkumarrana5567@gmail.com>
2026-07-06 13:15:53 +0530

    Initial repository structure

---
+++ 01-Variables-and-Data-Types/script.js
@@ -0,0 +1,357 @@
+//Level 1
+
+//Q.1 print name
+// let name = "vivek rana";
+// console.log(name);
+
+//Q.2 print my age is 25
+// let age = 25;
+// console.log("my age is",age);
+
+//Q.3 Store your city and state.
+// let city = "koderma";
+// let state = "jharkhand";
+// console.log(city);
+// console.log(state);
+
+//Q.4 Store two numbers and print their sum.
+// let nums1 = 20;
+// let num2 = 25;
+// console.log(nums1+num2);
+
+//Q.5 Store two numbers and print:
+//Addition
+//Subtraction
+//Multiplication
+//Division
+
+// let num1 = 63;
+// let num2 = 60;
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+
+//Q.6 Swap two variables using a third variable.
+
+// let a = "test";
+// let b = "temp";
+
+// let c = a;
+// let d = slow;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// console.log(a,b,c,d);
+
+
+//Q.7 Swap without using a third variable.
+
+// let a = 2;
+// let b = 3;
+
+// let c = a;
+// let d = a + b;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(a+b+c+d);
+// console.log(a,b,c,d);
+
+//Q.8 Store your favorite movie and print
+
+// let movie = "Laksya";
+// console.log("My favourite movie is", movie);
+
+//Q.9 Store your birth year and calculate your age.
+
+// let BirthAge = 2001;
+// let CurrentAge = 2026;
+// console.log(CurrentAge - BirthAge);
+
+//Q.10 Store the price of three products and print the total.
+
+// let pen = 10;
+// let pencil = 5;
+// let notebook = 20;
+
+// console.log(pen + pencil + notebook);
+
+//Q.11 Find the average of three numbers.
+
+// let a = 10;
+// let b = 30;
+// let c = 60;
+
+// console.log(a + b + c / 3);
+
+//Q.12 Store your college name.
+
+// let college = "Amity"
+// console.log(college);
+
+//Q13. Calculate the area of a rectangle.
+
+// let area = 31;
+// let width = 32;
+// console.log("Area of Rectangle =", area * width);
+
+//Q14. Calculate the perimeter of a rectangle.
+
+// let length = 20;
+// let width = 30;
+
+// console.log("perimeter of a rectangle", 2 * (length + width));
+
+//Q15. Calculate the area of a square.
+
+// let side = 63;
+
+// console.log(side * side);
+
+//Q16. Calculate the area of a circle.
+
+// let pie = 3.14;
+// let radius = 22;
+
+// console.log("The area of a Circle", pie * radius *radius);
+
+//Q17. Convert Celsius to Fahrenheit.
+
+// let Fahrenheit = 33;
+
+// console.log((Fahrenheit - 32) * 5 / 9);
+
+//Q19. Store marks of five subjects and calculate total.
+
+// let English = 98;
+// let Math = 90;
+// let Science = 80;
+// let Hindi = 88;
+// let Social = 96;
+
+// console.log(English + Math + Science + Hindi + Social);
+
+//Q20. Calculate percentage.
+
+// let nums = 96;
+// let num1 = 63;
+// let num2 = 60;
+// let num3 = 78;
+// let num4 = 96;
+// let num5 = 89;
+// let TotalNums = 1000;
+
+// console.log((nums + num1 + num2 + num3 + num4 + num5)/TotalNums * 100);
+
+
+//Level 2
+
+//Q21. Create a String.
+
+// let name = "Vivek";
+// console.log(typeof name);
+
+//Q22. Create a Number.
+
+// let number = 50;
+// console.log(typeof number);
+
+//Q23. Create a Boolean.
+
+// let mango = true;
+// console.log(typeof mango);
+
+//Q24. Create Undefined.
+
+// let city;
+// console.log(typeof city);
+
+//Q25. Create Null.
+
+// let phone = null;
+// console.log(phone);
+
+//Q26. Create an Object.
+
+// let objet ={
+//     firstName: "vivek",
+//     lastName: "Rana",
+//     age:25
+
+// }
+// console.log(objet);
+
+//Q27. Create an Array.
+
+// let arr = ["1",2,3,4,5,6, "Vivek"];
+
+// console.log(arr);
+
+// let skills = ["JavaScript", "HTML", "CSS"];
+// console.log(skills);
+
+//Q28. Print the type of every variable.
+
+// let String = "Vivek";
+// let number = 25;
+// let Boolean = true;
+
+// console.log(typeof String);
+// console.log(number);
+// console.log(Boolean);
+
+//Q29. Convert Number to String.
+
+// let age = 25;
+// console.log(String(age));
+
+// //Q30. Convert String to Number.
+
+// let name = "100";
+// console.log(Number(name));
+
+// //Q31. Convert Boolean to String.
+
+// let vivek = true;
+// console.log(String(vivek));
+
+//Q32. Convert String to Boolean.
+
+// let String = "vivek";
+// console.log(Boolean(String));
+
+//Q33. Check:
+
+// console.log(10 == "10");
+
+// //Q33. Check:
+// console.log(10 === "10");
+
+//Q35. Print all values of an object.
+
+// let student = {
+//     Name: "Vishal",
+//     Age: 24,
+//     skills: "Dotnet",
+//     adress: "Jainagar"
+// }
+
+// console.log(student.Name);
+// console.log(student.Age);
+// console.log(student.skills);
+// console.log(student.adress);
+
+//Q36. Print array elements.
+
+// let fruits  = ["Mango", "Apple", "Gauva", "Pine-Apple", "Grapes"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+//Q37. Create an object for your laptop.
+// let laptop ={
+//     Name: "Hp",
+//     Window: 11,
+//     Model: 2021
+// }
+// console.log(laptop);
+
+//Q38. Create an array of 5 cities.
+
+// let city = ["Delhi", 
+//     "Kolkata", 
+//     "Pune", 
+//     "Mumbai", 
+//     "Banglore"];
+// console.log(city);
+
+//Q39. Store your hobbies in an array.
+
+// let Hobbies = ["Reading Book", "coding", "Chess", "Music"];
+// console.log(Hobbies);
+
+//Q40. Create your complete profile.
+
+let myprofile ={
+    firstName: "Vivek",
+    lastName: "Rana",
+    Age: 25,
+    skills:[
+        "Mongodb",
+        "Express",
+        "React",
+        "Nodejs"
+    ],
+    Address:{
+        city: "Jainagar",
+        state: "Jharkhand",
+        pincode: 825109
+    }
+}
+console.log(myprofile);
+console.log(myprofile.Address.pincode);
+console.log(myprofile.skills);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
