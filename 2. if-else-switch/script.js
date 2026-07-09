7de48e3b791d649d2fdc907bebed0dd4a6d92d26
Vivek Kumar Rana <vivekkumarrana5567@gmail.com>
2026-07-09 12:11:24 +0530

    Added if-else-switch practice

---
+++ 2. if-else-switch/script.js
@@ -0,0 +1,205 @@
+//Q1. Check Positive, Negative or Zero
+// input = 10;
+// let nums = 10;
+// if(nums > 0){
+//     console.log("positive");
+// }else if(nums < 0){
+//     console.log("Negative");
+// }else{
+//     console.log("Zero");
+// }
+
+//Q2. Check Even or Odd
+// input = 7
+
+// let nums = 7;
+// if(nums % 2 === 0){
+//     console.log("Even");
+// }else{
+//     console.log("Odd");
+// }
+
+//Q3. Check Eligible to Vote
+
+// let age = 18;
+// if(age >= 18){
+//     console.log("Eligible");
+// }else{
+//     console.log("Not Eligible");
+// }
+
+//Q4. Find Greatest of Two Numbers
+// 10
+// 20
+
+// let firstNums = 10;
+// let secondNum = 20;
+
+// if(firstNums > secondNum){
+//     console.log(firstNums + " is smaller");
+
+// }else{
+//     console.log(secondNum + " is greater");
+// }
+
+//Q5. Pass or Fail
+//Marks = 35
+
+// let marks = 35;
+// if(marks >= 35){
+//     console.log("paas");
+// }else{
+//     console.log("Fail");
+// }
+
+
+//Q6. Divisible by 5
+//Input = 45
+
+// let nums = 45;
+// if(nums % 5 ===0){
+//     console.log("Divided by 5");
+    
+// }else{
+//     console.log("Not Divided by 5");
+// }
+
+//Q7. Find Smaller Number
+//12
+//8
+
+// let num1 = 12;
+// let num2 = 8;
+
+// if(num1 > num2){
+//     console.log(num1 + " is greater number");
+    
+// }else{
+//     console.log(num2 + " is smaller number");
+    
+// }
+
+//Q8. Check Driving License Eligibility
+//Age = 17
+
+// let age = 17;
+
+// if(age >= 18){
+//     console.log("Eligible for Driving Licence");
+    
+// }else{
+//     console.log("Not Eligible for Driving Licence");
+    
+// }
+
+//Q9. Check Number is Multiple of 10
+//Input = 50
+
+// let nums = 50;
+// if(nums % 10 === 0){
+//     console.log("Yes");
+// }else{
+//     console.log("No");
+// }
+
+//Q10. Grade System
+// 90+
+// A
+
+// 80+
+// B
+
+// 70+
+// C
+
+// 60+
+// D
+
+// Below 60
+// Fail
+
+// let nums = 95;
+// if(nums >= 90){
+//     console.log("A");
+// }else if(nums >= 80){
+//     console.log("B");
+// }else if(nums >= 70){
+//     console.log("C");
+// }else if(nums >= 60){
+//     console.log("D");
+// }else{
+//     console.log("Fail");
+// }
+
+// Q11. Check Leap Year
+
+// Input: let year = 2024;
+
+// let LeapYear = 2024;
+// if((LeapYear % 4 === 0 && LeapYear % 100 !== 0) 
+//     || LeapYear % 400 === 0){
+//     console.log("Leap Year");
+// }else{
+//     console.log("Not Leap year");
+// }
+
+// Q12. Largest of Three Numbers
+
+// Input:
+
+// let a = 20;
+// let b = 45;
+// let c = 35;
+
+// let a = 20;
+// let b = 45;
+// let c = 35;
+
+// if(a >= b && a >= c){
+//     console.log(a+ " is greater");
+    
+// }else if(b >= a && b >=c ){
+//     console.log(b+ " is greater");
+    
+// }else{
+//     console.log(c+ " is greater");
+    
+// }
+
+//Q13. Smallest of Three Numbers
+
+// Input:
+
+// let a = 20;
+// let b = 45;
+// let c = 35;
+
+// let a = 20;
+// let b = 45;
+// let c = 35;
+
+// if(a <=b && a <= c ){
+//     console.log(a + " is smaller");
+// }else if(b <= a && b <= c){
+//     console.log(b + " is smaller");
+// }else{
+//     console.log(c + " is smaller");
+// }
+
+// Q14. Check Character (Vowel or Consonant)
+
+// Input: let ch = "a";
+
+let ch = "a";
+if(
+    ch === "a" ||
+    ch === "e" ||
+    ch === "i" ||
+    ch === "o" ||
+    ch === "u"
+){
+    console.log("Vowel");
+}else{
+    console.log("Consonent");
+    
+}
